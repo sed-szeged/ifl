a42bef2d1abc3e82ee134f8f2c51bd213152d3e2
Ferenc Horváth <hferenc@inf.u-szeged.hu>
2017-09-14 10:52:50 +0200

    Clarify util functions

--- framework/utils.py
+++ framework/utils.py
@@ -1,17 +1,33 @@
 from glob import glob
-from os.path import join as j, isdir
 from os import remove
-from shutil import copy, rmtree
+from os.path import isdir
+from os.path import join as j
+from shutil import copy
+from shutil import rmtree
 
 
-def cp(source_dir, pattern, dst):
-    for file in glob(j(source_dir, pattern)):
-        copy(file, dst)
+def cp(source_dir, pattern, destination):
+    """
+    Copies items matching the given pattern from the source directory to the destination.
+
+    :param source_dir: source directory
+    :param pattern: glob pattern
+    :param destination: destination (may be a file)
+    """
+    for item in glob(j(source_dir, pattern)):
+        copy(item, destination)
 
 
-def rm(directory, pattern):
-    for file in glob(j(directory, pattern)):
-        if isdir(file):
-            rmtree(file)
+def rm(source_dir, pattern):
+    """
+    Removes items matching the given pattern from the source directory.
+    If the actual item is a directory then all its contents are removed recursively.
+
+    :param source_dir: source directory
+    :param pattern: glob pattern
+    """
+    for item in glob(j(source_dir, pattern)):
+        if isdir(item):
+            rmtree(item)
         else:
-            remove(file)
+            remove(item)
